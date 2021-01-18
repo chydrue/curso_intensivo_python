740eb7d20a71564ef9b9b08d172b2e71e3c778b5
Cleiton K. Morais <ckmoraiz@gmail.com>
2021-01-18 09:07:57 -0300

    Escrevendo mensagem para elementos da lista usando laço

--- Cap4_listas.py
+++ Cap4_listas.py
@@ -5,3 +5,14 @@
 magicians = ['crowley', 'fortune', 'motta']
 for magician in magicians:
     print(magician)
+
+###########################################################################################
+#Exibindo uma mensagem para cada mágico
+###########################################################################################
+
+magicians2 = ['grant', 'agrippa', 'salomão']
+for magician2 in magicians2:
+    print(magician2.title() + ", essa macumba foi muito boa!!")
+    print("Mal posso esperar para ver seu próximo truque, " + magician2.title() + ".\n")
+
+print("Obrigado a todos, está sendo uma aventura incrível! Amor é a Lei, Amor sob Vontade!")
