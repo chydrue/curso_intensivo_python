2ba561936de694515723a811e8b6b48913a0fe39
Cleiton K. Morais <ckmoraiz@gmail.com>
2021-01-18 09:40:26 -0300

    Exercício 4.2

--- Cap4_listas.py
+++ Cap4_listas.py
@@ -24,4 +24,13 @@ print('\n>>>Exercício 4.1<<<')
 pizzas = ['fragobacon', 'cheddar', 'calabresa']
 for pizza in pizzas:
     print('A pizza de ' + pizza + 'está em promoção hoje!')
-print('Vou comprar uma de cada para aproveitar!!!')
+print('Vou comprar uma de cada para aproveitar!!!\n')
+
+
+print('>>>Exercício 4.2<<<')
+gatos = ['max', 'eusébio', 'morpheu']
+for gato in gatos:
+    k = gatos.index(gato)+1
+    print('O ' + gato.title() + ' foi o {K}º animal de estimação a ser adotado!'.format(K=k))
+print("\nAdotar animais é muito bom!")
+
