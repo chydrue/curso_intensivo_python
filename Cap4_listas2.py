39e994fae818f38cc92325a679ced55f54e3473a
Cleiton K. Morais <ckmoraiz@gmail.com>
2021-01-18 10:15:46 -0300

    Exercício 4.3

--- Cap4_listas2.py
+++ Cap4_listas2.py
@@ -11,3 +11,12 @@ print('Menor valor:' + str(menor_valor) +'\nMaior valor' + str(maior_valor) + '\
 
 quadrados = [valor**2 for valor in range(1,11)]
 print('\nQuadrados: ' + str(quadrados))
+
+###########################################################################################
+#Exercícios 4.3 até 4.9
+###########################################################################################
+
+print('\n>>>Exercício 4.3<<<')
+ex43 = [valor for valor in list(range(1,21))]
+print(ex43)
+
