465da40cdaa112d9e5314f3421b671bae2210b70
Cleiton K. Morais <ckmoraiz@gmail.com>
2021-01-20 16:21:51 -0300

    Introdução aos dicionários.

---
+++ Cap6_dicionários.py
@@ -0,0 +1,11 @@
+########################################################################################################################
+#Dicionários são uma forma de armazenar diversos tipos de informação e relacioná-las através de palavas chave-valor.
+#Onde cada palavra chave é atribuída à um valor.
+########################################################################################################################
+
+print('Exemplo de dicionário:')
+alien_0={'cor': 'verde', 'pontos':'5'}
+print("A cor do alien é: " + alien_0['cor'] + ".")
+print("O alien possui " + alien_0['pontos'] + " pontos.")
+
+
